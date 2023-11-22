26c7e320d3b85df7a84e6e274f3be3815901cec3
Aaron Tan <aaronjtan00@gmail.com>
2023-11-21 19:44:12 -0500

    feat: create tables to store products, categories, brands

--- ecommerce-api/src/main/resources/schema.sql
+++ ecommerce-api/src/main/resources/schema.sql
@@ -23,3 +23,45 @@ CREATE TABLE user_roles (
   FOREIGN KEY (role_id) REFERENCES roles (id),
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE(name)
+);
+
+CREATE TABLE brands (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE(name)
+);
+
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL,
+    brand_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (brand_id) REFERENCES brands(id)
+);
+
+CREATE TABLE product_images (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    original_filename VARCHAR(255) NOT NULL,
+    system_filename VARCHAR(255) NOT NULL,
+    mimetype VARCHAR(255) NOT NULL,
+    display_order INT DEFAULT 0 NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
